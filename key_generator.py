013bc9909b4a78ba080853e8cdd884ac73b522a5
lees248 <lees248@mymacewan.ca>
2020-12-11 04:14:53 -0700

    Adding attempts for RSA & AES  encryption/decryption functions, update to key_generator.py and server*.pem files

--- key_generator.py
+++ key_generator.py
@@ -11,27 +11,26 @@ from Crypto.Cipher import PKCS1_OAEP
 #from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-def pubKeyGenerator():
-	key = RSA.generate(2048)
+def pubKeyGenerator(key):
 	public_key = key.publickey().export_key()
 	return public_key
 
-def privKeyGenerator():
-	keyLen = 256
-	key = get_random_bytes(int(keyLen/8))
-	return key
+def privKeyGenerator(key):
+	private_key = key.export_key()
+	return private_key
 	
-def pubkeyToFile(fname):
+	
+def pubkeyToFile(key, fname):
 	newFile = open(fname, "wb")
-	pubKey = pubKeyGenerator()
+	pubKey = pubKeyGenerator(key)
 	newFile.write(pubKey)
 	newFile.close()
 	return pubKey
 	
 
-def privkeyToFile(fname):
+def privkeyToFile(key, fname):
 	newFile = open(fname, "wb")
-	privKey = privKeyGenerator()
+	privKey = privKeyGenerator(key)
 	newFile.write(privKey)
 	newFile.close()
 	return privKey
@@ -43,21 +42,24 @@ def checkKeyFile(fname):
 	return content
 
 def showKeys():
-	pubkeyToFile("server_public.pem")
-	privkeyToFile("server_private.pem")
-
-	pubkeyToFile("client1_public.pem")
-	pubkeyToFile("client2_public.pem")
-	pubkeyToFile("client3_public.pem")
-	pubkeyToFile("client4_public.pem")
-	pubkeyToFile("client5_public.pem")
+	key = RSA.generate(2048)
 	
+	pubkeyToFile(key, "server_public.pem")
+	privkeyToFile(key, "server_private.pem")
+
+	pubkeyToFile(key, "client1_public.pem")
+	pubkeyToFile(key, "client2_public.pem")
+	pubkeyToFile(key, "client3_public.pem")
+	pubkeyToFile(key, "client4_public.pem")
+	pubkeyToFile(key, "client5_public.pem")
+	
+	'''
 	privkeyToFile("client1_private.pem")
 	privkeyToFile("client2_private.pem")
 	privkeyToFile("client3_private.pem")
 	privkeyToFile("client4_private.pem")
 	privkeyToFile("client5_private.pem")
-
+	'''
 	
 	
 #------
